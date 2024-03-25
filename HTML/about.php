ef40ca1456b67c5de4b6aae1bc9228378ab75cf1
Jozef <youssef.hamdani2000@gmail.com>
2024-03-25 23:52:25 +0100

    modified about-us page

--- HTML/about.php
+++ HTML/about.php
@@ -11,53 +11,50 @@
     
     <div class="about-container">
         <h2>About Us</h2>
-        
-        <!-- Author 1 -->
-        <div class="author">
+        <p class="about-paragraph1">
+            Carya.tn is a car rental service that offers a wide range of vehicles for rent. Our mission is to provide our customers with the best possible car rental experience, offering competitive rates, excellent customer service, and a wide selection of vehicles to choose from. Whether you need a car for a day, a week, or longer, we have the perfect vehicle for your needs. Our team of experienced professionals is dedicated to ensuring that you have a safe and enjoyable rental experience, from start to finish.
+        </p>
+            <div class="author">
             <img src="http://localhost/Mini-PHP-Project/Resources/Images/jozef.jpg" alt="Youssef Hamdani">
             <div class="author-info">
                 <h3>Youssef Hamdani</h3>
-                <p>Description of Youssef Hamdani</p>
+                <p>What am I doing here?</p>
             </div>
         </div>
         
-        <!-- Author 2 -->
         <div class="author">
             <img src="http://localhost/Mini-PHP-Project/Resources/Images/charfi.jpg" alt="Youssef Charfeddin">
             <div class="author-info">
                 <h3>Youssef Charfeddin</h3>
-                <p>Description of Youssef Charfeddin</p>
+                <p>5abbez</p>
             </div>
         </div>
 
-        <!-- Author 3 -->
         <div class="author">
             <img src="http://localhost/Mini-PHP-Project/Resources/Images/rayen.jpg" alt="Rayen Hamza">
             <div class="author-info">
                 <h3>Rayen Hamza</h3>
-                <p>Description of Rayen Hamza</p>
+                <p>Angenieur</p>
             </div>
         </div>
 
-        <!-- Author 4 -->
         <div class="author">
             <img src="http://localhost/Mini-PHP-Project/Resources/Images/mehdi.jpg" alt="Mehdi Lahouimel">
             <div class="author-info">
                 <h3>Mehdi Lahouimel</h3>
-                <p>Description of Mehdi Lahouimel</p>
+                <p>Harboucha</p>
             </div>
         </div>
 
-        <!-- Author 5 -->
         <div class="author">
             <img src="http://localhost/Mini-PHP-Project/Resources/Images/oussema.jpg" alt="Oussema Nehdi">
             <div class="author-info">
                 <h3>Oussema Nehdi</h3>
-                <p>Description of Oussema Nehdi</p>
+                <p>Ba77ar</p>
             </div>
         </div>
 
-        <p class="about-paragraph">
+        <p class="about-paragraph2">
             At carya.tn, we are a team of passionate individuals dedicated to delivering excellence in every aspect of our work. With a diverse range of skills and backgrounds, we come together to create innovative solutions and provide exceptional service to our clients. Our collaborative approach, coupled with a relentless pursuit of quality, drives us to exceed expectations and achieve success. We take pride in our commitment to integrity, professionalism, and continuous improvement, ensuring that we always strive for excellence in everything we do.
         </p>
     </div>
