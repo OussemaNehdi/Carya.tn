ceba2d2802545779b3c7ec09257202c7e976813e
Jozef <youssef.hamdani2000@gmail.com>
2024-03-26 00:51:50 +0100

    Added login and signup button to navbar

--- HTML/navbar.php
+++ HTML/navbar.php
@@ -1,4 +1,3 @@
-<!-- navbar.php -->
 <nav class="navbar">
     <div class="container">
         <div class="navbar-brand">
@@ -6,17 +5,21 @@
         </div>
         <div class="navbar-menu">
             <ul>
-                <li><a href="http://localhost/Mini-PHP-Project/">Home</a></li>
-                <li><a href="http://localhost/Mini-PHP-Project/HTML/about.php">About</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle">Services</a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Rent a Car</a></li>
-                        <li><a href="#">List Your Car for Rent</a></li>
-                        <li><a href="#">Export Rent History</a></li>
-                    </ul>
-                </li>
-                <li><a href="http://localhost/Mini-PHP-Project/HTML/contact.php">Contact</a></li>
+                <div class="basics">
+                    <li><a href="http://localhost/Mini-PHP-Project/">Home</a></li>
+                    <li><a href="http://localhost/Mini-PHP-Project/HTML/about.php">About</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Services</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Rent a Car</a></li>
+                            <li><a href="#">List Your Car for Rent</a></li>
+                            <li><a href="#">Export Rent History</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="http://localhost/Mini-PHP-Project/HTML/contact.php">Contact</a></li>
+                </div>
+                <li class="login-button"><a href="#">Login</a></li>
+                <li class="signup-button"><a href="#">Sign Up</a></li>
             </ul>
         </div>
     </div>
