9b6d9a95598da3fe7c5d9e8c375c5f16da664a71
nth <nothing@gmail.com>
2024-03-26 02:55:38 +0100

    filter functionnaility

--- PHP/fetch_cars.php
+++ PHP/fetch_cars.php
@@ -7,38 +7,69 @@
 <body>
     <?php
     require_once('connect.php');
-    $sql = "SELECT * FROM cars";
+    
+    // Constructing the SQL query based on filter criteria
+    $sql = "SELECT * FROM cars WHERE 1";
+    
+    // Filtering by brand
+    if (!empty($_GET['brand'])) {
+        $brands = implode("','", $_GET['brand']);
+        $sql .= " AND brand IN ('$brands')";
+    }
+
+    // Filtering by model
+    if (!empty($_GET['model'])) {
+        $models = implode("','", $_GET['model']);
+        $sql .= " AND model IN ('$models')";
+    }
+
+    // Filtering by color
+    if (!empty($_GET['color'])) {
+        $colors = implode("','", $_GET['color']);
+        $sql .= " AND color IN ('$colors')";
+    }
+
+    // Filtering by kilometers range
+    if (!empty($_GET['km_min']) || !empty($_GET['km_max'])) {
+        $km_min = $_GET['km_min'];
+        $km_max = $_GET['km_max'];
+        $sql .= " AND km BETWEEN $km_min AND $km_max";
+    }
+
+    // Filtering by price range
+    if (!empty($_GET['price_min']) || !empty($_GET['price_max'])) {
+        $price_min = $_GET['price_min'];
+        $price_max = $_GET['price_max'];
+        $sql .= " AND price BETWEEN $price_min AND $price_max";
+    }
+
     $result = $conn->query($sql);
     
-?>
-<h1>Rent a Car</h1>
-<p>Choose a car to rent:</p> 
-<table class="table table-striped m-0">
+    ?>
+    <h1>Rent a Car</h1>
+    <p>Choose a car to rent:</p> 
+    <table class="table table-striped m-0">
         <thead>
-        <tr>
-          <th>#</th>
-          <th>brand</th>
-          <th>model</th>
-          <th>color</th>
-          <th>price</th>
-
-        </tr>
+            <tr>
+                <th>#</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Color</th>
+                <th>Price</th>
+            </tr>
         </thead>
         <tbody>
             <?php while ($car = $result->fetch_assoc()) : ?>
-            <tr>
-                <th><?php echo $car['id']?> </th>
-                <td><?php echo $car['brand']?></td>
-                <td><?php echo $car['model']?></td>
-                <td><?php echo $car['color']?></td>
-                <td><?php echo $car['price']?></td>
-                <td><button class="btn btn-primary btn-sm">Rent</button></td>
-
-            </tr>
+                <tr>
+                    <td><?php echo $car['id']?> </td>
+                    <td><?php echo $car['brand']?></td>
+                    <td><?php echo $car['model']?></td>
+                    <td><?php echo $car['color']?></td>
+                    <td><?php echo $car['price']?></td>
+                    <td><button class="btn btn-primary btn-sm">Rent</button></td>
+                </tr>
             <?php endwhile; ?>
-      
-        
         </tbody>
-      </table>
+    </table>
 </body>
 </html>
--- PHP/rent_car.php
+++ PHP/rent_car.php
@@ -91,29 +91,29 @@
 <div class="container">
     <div class="filter-menu">
         <h2>Filter Menu</h2>
-        <form action="your_php_script.php" method="GET"> <!-- Change your_php_script.php to your actual PHP script -->
+        <form action="rent_car.php" method="GET">
             <h3>Brand</h3>
             <?php foreach ($brands as $brand): ?>
-                <label><input type="checkbox" name="brand[]" value="<?php echo $brand; ?>"><?php echo $brand; ?></label><br>
+                <label><input type="checkbox" name="brand[]" value="<?php echo $brand; ?>" <?php if (isset($_GET['brand']) && in_array($brand, $_GET['brand'])) echo 'checked'; ?>><?php echo $brand; ?></label><br>
             <?php endforeach; ?>
 
             <h3>Model</h3>
             <?php foreach ($models as $model): ?>
-                <label><input type="checkbox" name="model[]" value="<?php echo $model; ?>"><?php echo $model; ?></label><br>
+                <label><input type="checkbox" name="model[]" value="<?php echo $model; ?>" <?php if (isset($_GET['model']) && in_array($model, $_GET['model'])) echo 'checked'; ?>><?php echo $model; ?></label><br>
             <?php endforeach; ?>
 
             <h3>Color</h3>
             <?php foreach ($colors as $color): ?>
-                <label><input type="checkbox" name="color[]" value="<?php echo $color; ?>"><?php echo $color; ?></label><br>
+                <label><input type="checkbox" name="color[]" value="<?php echo $color; ?>" <?php if (isset($_GET['color']) && in_array($color, $_GET['color'])) echo 'checked'; ?>><?php echo $color; ?></label><br>
             <?php endforeach; ?>
 
             <h3>Kilometers Range</h3>
-            <input type="range" id="km_min" name="km_min" min="0" max="<?php echo $max_km; ?>" value="0"> <span id="km_min_val">0</span> km<br>
-            <input type="range" id="km_max" name="km_max" min="0" max="<?php echo $max_km; ?>" value="<?php echo $max_km; ?>"> <span id="km_max_val"><?php echo $max_km; ?></span> km<br>
+            <input type="range" id="km_min" name="km_min" min="0" max="<?php echo $max_km; ?>" value="<?php echo isset($_GET['km_min']) ? $_GET['km_min'] : '0'; ?>"> <span id="km_min_val"><?php echo isset($_GET['km_min']) ? $_GET['km_min'] : '0'; ?></span> km<br>
+            <input type="range" id="km_max" name="km_max" min="0" max="<?php echo $max_km; ?>" value="<?php echo isset($_GET['km_max']) ? $_GET['km_max'] : $max_km; ?>"> <span id="km_max_val"><?php echo isset($_GET['km_max']) ? $_GET['km_max'] : $max_km; ?></span> km<br>
 
             <h3>Price Range</h3>
-            <input type="range" id="price_min" name="price_min" min="0" max="<?php echo $max_price; ?>" value="0"> $<span id="price_min_val">0</span><br>
-            <input type="range" id="price_max" name="price_max" min="0" max="<?php echo $max_price; ?>" value="<?php echo $max_price; ?>"> $<span id="price_max_val"><?php echo $max_price; ?></span><br>
+            <input type="range" id="price_min" name="price_min" min="0" max="<?php echo $max_price; ?>" value="<?php echo isset($_GET['price_min']) ? $_GET['price_min'] : '0'; ?>"> $<span id="price_min_val"><?php echo isset($_GET['price_min']) ? $_GET['price_min'] : '0'; ?></span><br>
+            <input type="range" id="price_max" name="price_max" min="0" max="<?php echo $max_price; ?>" value="<?php echo isset($_GET['price_max']) ? $_GET['price_max'] : $max_price; ?>"> $<span id="price_max_val"><?php echo isset($_GET['price_max']) ? $_GET['price_max'] : $max_price; ?></span><br>
 
             <input type="submit" value="Apply Filters">
         </form>
