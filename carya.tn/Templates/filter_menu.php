58212dc7cf47dca4bd4494a8e2e313418eee6658
Jozef <youssef.hamdani2000@gmail.com>
2024-04-01 00:58:55 +0100

    Medium Change: Styled the rent_car.php and fixed the floating filter menu and styled it also

--- carya.tn/Templates/filter_menu.php
+++ carya.tn/Templates/filter_menu.php
@@ -19,7 +19,7 @@ function generateInputCheckboxes($data, $filterData, $filterName) {
 function generateRangeInput($id, $name, $min, $max, $value, $filterData) {
     ?>
     <!-- Uses the range selected by the user and displays it -->
-    <input type="range" id="<?php echo $id; ?>" name="<?php echo $name; ?>" min="<?php echo $min; ?>" max="<?php echo $max; ?>" value="<?php echo isset($filterData[$name]) ? $filterData[$name][0] : $value; ?>" oninput="updateRangeValue('<?php echo $id; ?>', '<?php echo $id; ?>_val', this.value)"> 
+    <input class="price-range-slider" type="range" id="<?php echo $id; ?>" name="<?php echo $name; ?>" min="<?php echo $min; ?>" max="<?php echo $max; ?>" value="<?php echo isset($filterData[$name]) ? $filterData[$name][0] : $value; ?>" oninput="updateRangeValue('<?php echo $id; ?>', '<?php echo $id; ?>_val', this.value)"> 
     <span id="<?php echo $id; ?>_val"><?php echo isset($filterData[$name]) ? $filterData[$name][0] : $value; ?></span>
     <?php
 }
@@ -33,43 +33,41 @@ function isChecked($value, $filterData, $filterName) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/Mini-PHP-Project/CSS/style.css">
-    <title>Rental Car Website</title>
-</head>
-<body class="rent-body">
-    <div class="container">
-        <div class="filter-menu">
-            <h2>Filter Menu</h2>
-            <form action="http://localhost/Mini-PHP-Project/carya.tn/src/controllers/filter_controller.php" method="POST">
-                <h3>Brand</h3>
-                <?php generateInputCheckboxes($brands, $filter_data, 'brand'); ?>
-
-                <h3>Model</h3>
-                <?php generateInputCheckboxes($models, $filter_data, 'model'); ?>
-
-                <h3>Color</h3>
-                <?php generateInputCheckboxes($colors, $filter_data, 'color'); ?>
-
-                <h3>Kilometers Range</h3>
-                <?php generateRangeInput('km_min', 'km_min', 0, $max_km, 0, $filter_data); ?> km<br>
-                <?php generateRangeInput('km_max', 'km_max', 0, $max_km, $max_km, $filter_data); ?> km<br>
-
-                <h3>Price Range</h3>
-                <?php generateRangeInput('price_min', 'price_min', 0, $max_price, 0, $filter_data); ?> $<br>
-                <?php generateRangeInput('price_max', 'price_max', 0, $max_price, $max_price, $filter_data); ?> $<br>
-                <input type="submit" value="Apply Filters">
-            </form>
-        </div>
+<div class="container">
+    <div class="titles">
+        <h2>Filter Menu</h2>
     </div>
-    <script>
-        function updateRangeValue(rangeId, spanId, newValue) {
-            document.getElementById(spanId).innerText = newValue;
-        }
-    </script>
-</body>
-</html>
+    <form action="http://localhost/Mini-PHP-Project/carya.tn/src/controllers/filter_controller.php" method="POST">
+        <div class="sub-container">
+            <h3>Brand</h3>
+            <?php generateInputCheckboxes($brands, $filter_data, 'brand'); ?>
+        </div>
+        <div class="sub-container">
+            <h3>Model</h3>
+            <?php generateInputCheckboxes($models, $filter_data, 'model'); ?>
+        </div>
+
+        <div class="sub-container">
+            <h3>Color</h3>
+            <?php generateInputCheckboxes($colors, $filter_data, 'color'); ?>
+        </div>
+
+        <div class="sub-container">
+            <h3>Km Range</h3>
+            <?php generateRangeInput('km_min', 'km_min', 0, $max_km, 0, $filter_data); ?> <span>km</span><br>
+            <?php generateRangeInput('km_max', 'km_max', 0, $max_km, $max_km, $filter_data); ?> <span>km</span><br>
+        </div>
+
+        <div class="sub-container">
+            <h3>Price Range</h3>
+            <?php generateRangeInput('price_min', 'price_min', 0, $max_price, 0, $filter_data); ?><span>$</span><br>
+            <?php generateRangeInput('price_max', 'price_max', 0, $max_price, $max_price, $filter_data); ?><span>$</span><br>
+            <input class="apply-button" type="submit" value="Apply Filters">
+        </div>
+    </form>
+</div>
+<script>
+    function updateRangeValue(rangeId, spanId, newValue) {
+        document.getElementById(spanId).innerText = newValue;
+    }
+</script>
