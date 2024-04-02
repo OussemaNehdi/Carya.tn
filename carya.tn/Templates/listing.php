93125f7ee9ec70d42be31d27d629fb97866b1c96
siBouZitoun <gumball.daruine@gmail.com>
2024-04-02 21:09:41 -0100

    fix

--- carya.tn/Templates/listing.php
+++ carya.tn/Templates/listing.php
@@ -73,7 +73,7 @@ ob_start();
 </div>
 
 <?php foreach ($cars as $car): ?>
-<?require('update_form.php'); ?>
+<?php require('update_form.php'); ?>
 <?php $commands = Command::getRentalCommandsByCarId($car->id); ?>
 <?php require('confirm_command_popup.php'); ?>
 <?php endforeach; ?>
